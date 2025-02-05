d8c9ddffb7dac24c4381df716472803c5d0ced74
missayev <feeerhd.342@gmail.com>
2025-02-05 22:19:40 +0500

    fix: name of dependencies

--- tablemate_api/main.py
+++ tablemate_api/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from tablemate_api.routes.auth_routes import router as auth_router
 from tablemate_api.routes.protected_routes import router as protected_router
+from tablemate_api.middleware import add_cors
 
 app = FastAPI()
-
+add_cors(app)
 # Include authentication and protected routes
 app.include_router(auth_router)
 app.include_router(protected_router)
